9ce95cc5db197f3ca2e26fd9de6a698d115f09a9
Travis Lawrence <travis.lawrence12@gmail.com>
2014-02-03 08:16:19 -0800

    keycodes light up

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,16 @@
+var noteDown = function(key){
+  $('#' + keyDict[key]).addClass('keydown');
+}
+
+var noteUp = function(key){
+  $('#' + keyDict[key]).removeClass('keydown');
+}
+
 $(document).ready(function(){
   $( "body" ).keydown(function(e) {
-    console.log(e.keyCode);
+    e.preventDefault();
+    noteDown(e.keyCode);
+  }).keyup(function(e){
+    noteUp(e.keyCode);
   });
 })
