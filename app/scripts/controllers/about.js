e16a5a69f6be6fbe9dc836a0e3b90ef354a23065
TravisL12 <travis.lawrence12@gmail.com>
2015-12-29 13:23:02 -0800

    Using Yeoman now

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name keyIanoNewApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the keyIanoNewApp
+ */
+angular.module('keyIanoNewApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
