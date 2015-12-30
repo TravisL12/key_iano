b7a00bd8f09cb46f0ef34d5521bdbda47a1e7092
TravisL12 <travis.lawrence12@gmail.com>
2015-12-29 21:39:17 -0800

    Keys beginning to work right, looking good

--- app/scripts/controllers/keyboard.js
+++ app/scripts/controllers/keyboard.js
@@ -11,25 +11,27 @@ angular.module('keyIanoNewApp')
   .controller('KeyboardCtrl', function ($scope, Notes, KeyboardLayout) {
     $scope.keyboardLayout = KeyboardLayout;
 
-    function playSound(e, note) {
-      if (note && note.sound) {
-        // e.target.className = 'keydown';
-        note.sound.play();
-        note.sound._onend[0] = function() {
-          // e.target.className = '';
+    function playSound(e, key) {
+      if (key.note && key.note.sound) {
+        key.active = true;
+        key.note.sound.play();
+        key.note.sound._onend[0] = function() {
+          key.active = false;
         };
       }
     }
 
     $scope.noteClicked = function(e) {
-      var note = Notes.charLookup(this.key.key);
-      playSound(e, note);
+      var key = this.key;
+      key.note = Notes.charLookup(key.key.toLowerCase());
+      playSound(e, key);
     };
 
     $scope.notePressed = function(e){
       e.preventDefault();
-      var note = Notes.codeLookup(e.keyCode);
-      playSound(e, note);
+      var key = {};
+      key.note = Notes.codeLookup(e.keyCode);
+      playSound(e, key);
     };
 
   });
